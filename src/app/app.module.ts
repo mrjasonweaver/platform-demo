52ec6f0c09aa214b7537f52c8ac4783e5cef128d
James Weaver <indyplanets@gmail.com>
2018-05-10 19:55:52 -0500

    app shell and dashboard components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
+import { MatToolbarModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HeaderComponent } from './app-shell/header/header.component';
+import { LogoComponent } from './app-shell/logo/logo.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HeaderComponent,
+    LogoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    RouterModule.forRoot([
+      // routes
+      { path: '', pathMatch: 'full', component: DashboardComponent }
+    ], {useHash: true}),
   ],
   providers: [],
   bootstrap: [AppComponent]
